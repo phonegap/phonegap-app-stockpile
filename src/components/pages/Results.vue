f65111e5a6aa17993e3cd6d3aaef2717935d9074
tommy-carlos williams <tommy@devgeeks.org>
2017-06-23 15:29:09 +1000

    :sparkles: Added F7 PhotoBrowser for results
    We will want to also add a custom navbar so we can have a favourites button

--- src/components/pages/Results.vue
+++ src/components/pages/Results.vue
@@ -6,10 +6,13 @@
     <f7-block>
       <f7-grid v-for="i in Math.ceil(images.length / 3)" key="i">
         <f7-col width="33" v-for="image in images.slice((i - 1) * 3, i * 3)" key="image.id">
-          <img :src="image.thumbnail_url" style="width: 100%" />
+          <img @click="() => onImageClick(image.id)" :src="image.thumbnail_url" style="width: 100%" />
         </f7-col>
       </f7-grid>
     </f7-block>
+    <!-- Photo Browser -->
+    <!-- @TODO Add custom navbar template to allow Favorites -->
+    <f7-photo-browser ref="photoBrowser" :photos="photos"></f7-photo-browser>
   </f7-page>
 </template>
 
@@ -53,12 +56,35 @@
             console.log('fetching failed', ex);
             this.$f7.hidePreloader();
           });
+      },
+      onImageClick (id) {
+        const index = this.photos.findIndex(element => {
+          return element.id === id;
+        });
+        console.log(index);
+        const albumPhotoBrowser = this.$f7.photoBrowser({
+          photos: this.photos,
+          type: 'page',
+          backLinkText: 'Results',
+          lazyLoading: true,
+          initialSlide: index || 0
+        });
+        albumPhotoBrowser.open();
       }
     },
     computed: {
       imagesReturned () {
         const { q } = this.$route.params;
         return this.images ? `${this.images.length} reults for "${q}"` : '';
+      },
+      photos () {
+        return this.images.map(image => {
+          return {
+            id: image.id,
+            url: image.thumbnail_url,
+            caption: image.title
+          };
+        });
       }
     },
     created () {
