2723a10f0448a1daaa40f156320238e5ec27a250
tommy-carlos williams <tommy@devgeeks.org>
2017-06-23 13:01:00 +1000

    :art: Moved style from inline to component style tag

--- src/components/pages/Results.vue
+++ src/components/pages/Results.vue
@@ -4,7 +4,7 @@
     <!-- Scrollable page content-->
     <f7-block-title>{{ imagesReturned }}</f7-block-title>
     <f7-block>
-      <f7-grid style="margin-bottom: 5px;" v-for="i in Math.ceil(images.length / 3)" key="i">
+      <f7-grid v-for="i in Math.ceil(images.length / 3)" key="i">
         <f7-col width="33" v-for="image in images.slice((i - 1) * 3, i * 3)" key="image.id">
           <img :src="image.thumbnail_url" style="width: 100%" />
         </f7-col>
@@ -28,7 +28,7 @@
         const apiURL =
           `https://stock.adobe.io/Rest/Media/1/Search/Files?search_parameters[limit]=${limit}&search_parameters[${filter}]=${q}`;
         const myHeaders = new Headers({
-          'x-api-key': '***************************', // replace with your api-key
+          'x-api-key': '35600b72b7b04b8fb240dfb952058eb8', // replace with your api-key
           'X-Product': 'Stockpile/0.0.1'
         });
         console.log(filter);
@@ -68,3 +68,9 @@
     }
   };
 </script>
+
+<style scoped>
+  .row {
+    margin-bottom: 5px;
+  }
+</style>
