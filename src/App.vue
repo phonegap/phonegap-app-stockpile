7c03aa844ecce57b44884864fa4508379ae3e24a
tommy-carlos williams <tommy@devgeeks.org>
2017-07-14 11:45:50 +1000

    :art: Moved backbutton event handling to App.vue

--- src/App.vue
+++ src/App.vue
@@ -30,12 +30,34 @@
 </template>
 
 <script>
+  /* global window document */
   import LeftPanel from './components/LeftPanel';
 
   export default {
     name: 'App',
     components: {
       'left-panel-view': LeftPanel
+    },
+    methods: {
+      handleBackButton () {
+        // NOTE: The back button will behave differently depending on circumstance
+        // If the side panel is open, close it
+        if (document.querySelector('.panel-left.active')) {
+          // This will do nothing when the split-view is open
+          return this.$f7.closePanel();
+        }
+        // Close modals
+        // @TODO How to handle modals we shouldn't close like a login-screen?
+        if (document.querySelector('.modal-in')) {
+          return this.$f7.closeModal();
+        }
+        // If we have a back button, we want it to go back
+        if (this.$f7.mainView.history.length > 1) {
+          return this.$f7.mainView.router.back();
+        }
+        // Default to closing the app
+        return window.navigator.app.exitApp();
+      }
     },
     computed: {
       isiOS () {
@@ -44,6 +66,9 @@
       platformClass () {
         return window.isiOS ? 'ios' : 'material';
       }
+    },
+    created () {
+      document.addEventListener('backbutton', this.handleBackButton);
     }
   };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-/* global window document f7 */
+/* global window document */
 
 import 'babel-polyfill';
 
@@ -66,32 +66,6 @@ new Vue({ // eslint-disable-line no-new
   components: {
     app: App
   }
-});
-
-function handleBackButton () {
-  // NOTE: The back button will behave differently depending on circumstance
-  // If the side panel is open, close it
-  if (document.querySelector('.panel-left.active')) {
-    // This will do nothing when the split-view is open
-    return window.f7.closePanel();
-  }
-  // Close modals
-  // @TODO How to handle modals we shouldn't close like a login-screen?
-  if (document.querySelector('.modal-in')) {
-    return f7.closeModal();
-  }
-  // If we have a back button, we want it to go back
-  if (f7.mainView.history.length > 1) {
-    return f7.mainView.router.back();
-  }
-  // Default to closing the app
-  return window.navigator.app.exitApp();
-}
-
-// Ye olde Device Ready
-document.addEventListener('deviceready', () => {
-  // Bind to the back button for Android
-  document.addEventListener('backbutton', handleBackButton);
 });
 
 // Set up a global store
