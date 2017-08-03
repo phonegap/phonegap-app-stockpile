9c4d45c21bdacce0227c4679108aa74511948ee7
tommy-carlos williams <tommy@devgeeks.org>
2017-08-03 20:51:13 +1000

    :bug: *facepalm*

--- config/index.js
+++ config/index.js
@@ -8,12 +8,12 @@ module.exports = {
     assetsRoot: path.resolve(__dirname, '../www'),
     assetsSubDirectory: 'static',
     assetsPublicPath: './',
-    productionSourceMap: false,
+    productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
     // Before setting to `true`, make sure to:
     // npm install --save-dev compression-webpack-plugin
-    productionGzip: true,
+    productionGzip: false,
     productionGzipExtensions: ['js', 'css'],
     // Run the build command with an extra argument to
     // View the bundle analyzer report after build finishes:
